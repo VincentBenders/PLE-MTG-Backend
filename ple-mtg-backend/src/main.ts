07647dc535bb7e3d41ff2c1a57f8bf0e6f3c1a3a
vincent benders <vincentbenders@gmail.com>
2026-05-19 15:21:38 +0200

    Update main.ts

--- ple-mtg-backend/src/main.ts
+++ ple-mtg-backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('Server started on port', process.env.PORT);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
